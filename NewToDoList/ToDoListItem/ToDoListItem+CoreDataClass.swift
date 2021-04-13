3e8f96ce6eda3b1ff247d993fda528d0497f6d88
Foo Jia Jieng <foojiajieng@Foos-MacBook-Air.local>
2021-04-13 14:48:27 +0800

    first commit

---
+++ NewToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  NewToDoList
+//
+//  Created by Foo Jia Jieng on 12/04/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
